60d3a1f205d73d88519601db4f0c6c4f6e32401b
Ronald Hernandez <2ronald.hernandez@gmail.com>
2017-01-15 22:24:43 -0600

    added comments to pass data from one viewController to a DetailsViewController

--- BasicSample/Basic Sample/ListViewController.h
+++ BasicSample/Basic Sample/ListViewController.h
@@ -10,4 +10,8 @@
 
 @interface ListViewController : UIViewController
 
+// here you want to have property to store the event/whatever object you need to pass to this view controller. I would keep all the labels, textviews and other subviews private. So keep them in the .m file. Use the property you declare here to fill out the information you need for those views.
+//@property (nonatomic) SomeTypeObject *variableName
+//note this property will be visible to other files.
+
 @end
--- BasicSample/Basic Sample/ViewController.h
+++ BasicSample/Basic Sample/ViewController.h
@@ -2,6 +2,7 @@
 
 #import <UIKit/UIKit.h>
 #import <Gimbal/Gimbal.h>
+#import "ListViewController.h" // you'll need to import this guy to get a reference. 
 
 @interface ViewController : UIViewController
 
